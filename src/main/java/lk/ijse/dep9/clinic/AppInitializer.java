4e0aa5e3244f6f2e16c83344d943d534a543fe70
danulatb <balasooriyadt@gmail.com>
2022-09-02 13:28:47 +0530

    Load Login Form at the startup

--- src/main/java/lk/ijse/dep9/clinic/AppInitializer.java
+++ src/main/java/lk/ijse/dep9/clinic/AppInitializer.java
@@ -1,7 +1,13 @@
 package lk.ijse.dep9.clinic;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class AppInitializer extends Application {
 
@@ -10,7 +16,16 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+
+        URL resource = this.getClass().getResource("/view/LoginForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+
+        Scene scene = new Scene(load);
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.setTitle("Login Form");
+        primaryStage.show();
 
     }
 }
