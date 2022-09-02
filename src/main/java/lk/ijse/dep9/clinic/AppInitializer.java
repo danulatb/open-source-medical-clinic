c578ffad918e3a5f24ef02afd0f7f3c8ed4b01f3
danulatb <balasooriyadt@gmail.com>
2022-09-02 12:16:59 +0530

    Close #2

---
+++ src/main/java/lk/ijse/dep9/clinic/AppInitializer.java
@@ -0,0 +1,15 @@
+package lk.ijse.dep9.clinic;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
