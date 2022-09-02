e35c451f7963526ca66cc3693edb3872905a7b78
danulatb <balasooriyadt@gmail.com>
2022-09-02 13:40:25 +0530

    Validation logic for login form

--- src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep9.clinic.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -10,5 +11,28 @@ public class LoginFormController {
     public Button btnLogin;
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
+
+        String userName = txtUserName.getText();
+        String password = txtPassword.getText();
+        
+        if (userName.isBlank()){
+            new Alert(Alert.AlertType.ERROR,"User Name can not be empty").showAndWait();
+            txtUserName.requestFocus();
+        }else if (password.isBlank()){
+            new Alert(Alert.AlertType.ERROR,"Password can not beb empty").showAndWait();
+            txtPassword.requestFocus();
+        } else if (!userName.matches("^[A-Za-z0-9]+$")) {
+            new Alert(Alert.AlertType.ERROR,"Invalid username").showAndWait();
+
+        }
+
     }
+    
+    
+    public void initialize(){
+        btnLogin.setDefaultButton(true);
+
+    }
+    
+    
 }
