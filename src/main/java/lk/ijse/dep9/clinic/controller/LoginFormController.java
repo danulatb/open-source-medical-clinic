9f77b1614da43294890a6291957241ae6c1b0130
danulatb <balasooriyadt@gmail.com>
2022-09-02 13:16:35 +0530

    UIs Done

--- src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
@@ -1,4 +1,14 @@
 package lk.ijse.dep9.clinic.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+    public TextField txtUserName;
+    public TextField txtPassword;
+    public Button btnLogin;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
