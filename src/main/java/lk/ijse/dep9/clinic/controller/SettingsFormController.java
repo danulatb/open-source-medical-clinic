7e8e31357ff87d37769726df9975d79b6b81dcb7
danulatb <balasooriyadt@gmail.com>
2022-09-05 17:18:49 +0530

    UI start

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/SettingsFormController.java
@@ -0,0 +1,20 @@
+package lk.ijse.dep9.clinic.controller;
+
+import javafx.event.ActionEvent;
+
+public class SettingsFormController {
+    public void btnManagePricesOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDiscontsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageFieldsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnChangegPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+    }
+}
