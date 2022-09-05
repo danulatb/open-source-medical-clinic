ffb0b9fb2ef785d4b581f95925a7977f4c7c0e30
danulatb <balasooriyadt@gmail.com>
2022-09-05 16:54:47 +0530

    UI basic

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
@@ -1,4 +1,17 @@
 package lk.ijse.dep9.clinic.controller;
 
+import javafx.event.ActionEvent;
+
 public class AdminDashboardFormController {
+    public void btnProfileManagmentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewRecordsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSettingsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) {
+    }
 }
