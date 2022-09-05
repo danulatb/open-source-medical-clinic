918865192fd1e80c5c9ba31f5a915f15a4890169
danulatb <balasooriyadt@gmail.com>
2022-09-05 18:20:43 +0530

    navigate to add profile

--- src/main/java/lk/ijse/dep9/clinic/controller/ProfileManagementFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/ProfileManagementFormController.java
@@ -1,8 +1,13 @@
 package lk.ijse.dep9.clinic.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ProfileManagementFormController {
     public TableView tblDetails;
@@ -10,7 +15,16 @@ public class ProfileManagementFormController {
     public Button btnModifyProfile;
     public Button btnDeleteProfile;
 
-    public void btnAddProfileOnAction(ActionEvent actionEvent) {
+    public void btnAddProfileOnAction(ActionEvent actionEvent) throws IOException {
+        Scene scene = FXMLLoader.load(this.getClass().getResource("/view/AddProfileForm.fxml"));
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Add New Profile");
+        stage.show();
+
+
     }
 
     public void btnModifyProfileOnAction(ActionEvent actionEvent) {
