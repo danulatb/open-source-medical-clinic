2c19e55c3c7c62451aa3792a6fbe0c707646683b
danulatb <balasooriyadt@gmail.com>
2022-09-05 17:09:26 +0530

    UI basic

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/ProfileManagementFormController.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep9.clinic.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
+public class ProfileManagementFormController {
+    public TableView tblDetails;
+
+    public void btnAddProfileOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModifyProfileOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteProfileOnAction(ActionEvent actionEvent) {
+    }
+}
