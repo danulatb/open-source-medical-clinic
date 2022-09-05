1e8754572d258743b8dd77e6ca9d4e19f83191e1
danulatb <balasooriyadt@gmail.com>
2022-09-05 17:52:27 +0530

    Manage Fields UI

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/ManageFieldFormController.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep9.clinic.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ManageFieldFormController {
+    public AnchorPane pneManageFields;
+    public TextField txtAddField;
+    public ListView lstFields;
+    public Button btnAdd;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+}
