2eedd141f9f5d78eed8dcf3af760868a85d2938a
danulatb <balasooriyadt@gmail.com>
2022-09-05 17:43:27 +0530

    Second UI

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/AddProfileFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.dep9.clinic.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddProfileFormController {
+    public ComboBox cmbPriofileType;
+    public TextField txtName;
+    public TextField txtUserName;
+    public ComboBox cmbField;
+    public ComboBox cmbGender;
+    public TextField txtNIC;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public Button btnSave;
+    public Button btnReset;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnResetOnAction(ActionEvent actionEvent) {
+    }
+}
