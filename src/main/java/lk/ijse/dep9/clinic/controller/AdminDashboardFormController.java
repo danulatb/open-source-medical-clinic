7c3deac54ef2957500746753682bfb3b4a7ef8b0
danulatb <balasooriyadt@gmail.com>
2022-09-07 12:11:15 +0530

    Close #7

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
@@ -1,6 +1,11 @@
 package lk.ijse.dep9.clinic.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminDashboardFormController {
     public void btnProfileManagmentOnAction(ActionEvent actionEvent) {
@@ -9,7 +14,16 @@ public class AdminDashboardFormController {
     public void btnViewRecordsOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnSettingsOnAction(ActionEvent actionEvent) {
+    public void btnSettingsOnAction(ActionEvent actionEvent) throws IOException {
+
+        Scene scene = new Scene(FXMLLoader.load(this.getClass().getResource("/view/SettingsForm.fxml")));
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Settings");
+        stage.show();
+
     }
 
     public void btnLogOutOnAction(ActionEvent actionEvent) {
