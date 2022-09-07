a977fd12277029825173d66258774c683cfe8b97
danulatb <balasooriyadt@gmail.com>
2022-09-07 12:17:00 +0530

    resolve solved

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -28,7 +30,16 @@ public class AdminDashboardFormController {
     public void btnViewRecordsOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnSettingsOnAction(ActionEvent actionEvent) {
+    public void btnSettingsOnAction(ActionEvent actionEvent) throws IOException {
+
+        Scene scene = new Scene(FXMLLoader.load(this.getClass().getResource("/view/SettingsForm.fxml")));
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Settings");
+        stage.show();
+
     }
 
     public void btnLogOutOnAction(ActionEvent actionEvent) throws IOException {
